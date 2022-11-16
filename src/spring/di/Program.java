b999c101ccfe5066b8015703000b0243dc0ece3c
Jaegyeong Kim <dongji_11@naver.com>
2022-11-16 19:20:23 +0900

    일곱 번째 커밋 - 컬렉션 생성과 목록DI, 생성자로 컬렉션에 값 add하기

--- src/spring/di/Program.java
+++ src/spring/di/Program.java
@@ -1,5 +1,8 @@
 package spring.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -24,19 +27,27 @@ public class Program {
 		
 		console.print();
 	*/
-		
-		// context가 지시서(setting.xml)를 잘 읽었다면, 지시서에 있는 내용에 따라 객체를 생성하고 di까지 해서 ioc컨테이너에 잘 담아두었을 것임.
-		// setting.xml에 있는 id를 통해서 꺼내 사용할 수 도 있고, class를 이용할 수도 있음
+	
 		ApplicationContext context =
 				new ClassPathXmlApplicationContext("spring/di/setting.xml");
-		Exam exam = context.getBean(Exam.class); // Bean객체를 가져오기 
-		//이름으로 getBean을 하면, 캐스팅을 해야 함. why? 자료형을 알지 못하니까 context에서 꺼내올 때 object타입으로 꺼내오기 때문에 
-		//ExamConsole console = (ExamConsole) context.getBean("console");
-		
-		//자료형으로 꺼내기, 선호되는 방법  
+		Exam exam = context.getBean(Exam.class);
 		ExamConsole console = context.getBean(ExamConsole.class); 
-		System.out.println(exam.toString());
-		console.print();
+		//console.print();
+		
+		//콜렉션 생성과 목록 DI. arrayList를 xml에서 생성하여 값을 초기과할 수 있는 방법을 배움
+		
+		//List<Exam> exams= new ArrayList<>(); 
+		List<Exam> exams = (List<Exam>) context.getBean("exams");
+		/*context.getBean("exams")가 new ArrayList<>()와 같은 역할을 하는 이유는 
+		setting.xml 에 exams라는 id의 bean 객체에 class="java.util.ArrayList" 
+		으로 지정이 되어 있기 때문이다. 
+		*/
+		exams.add(new NewlecExam(1,1,1,1));
+		
+		
+		for(Exam e : exams) {
+			System.out.println(e);
+		}
 	}
 
 }
